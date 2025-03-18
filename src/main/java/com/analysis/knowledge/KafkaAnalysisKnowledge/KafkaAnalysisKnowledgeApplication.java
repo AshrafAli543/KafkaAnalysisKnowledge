ee9371a7d2323d9a2b665f0baf8aa6bcd9fcaae8
Ashraf Ali.Shaik <ashrafalis711@gmail.com>
2025-03-18 16:11:19 +0530

    Intial commit
    Intial commit

---
+++ src/main/java/com/analysis/knowledge/KafkaAnalysisKnowledge/KafkaAnalysisKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.analysis.knowledge.KafkaAnalysisKnowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAnalysisKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAnalysisKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analysis/knowledge/KafkaAnalysisKnowledge/KafkaAnalysisKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analysis.knowledge.KafkaAnalysisKnowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAnalysisKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
